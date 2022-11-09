cd77a6d082473b88d6d7b7730e46ac098e72b470
Alan Hernández <ahernandez102@ucol.mx>
2022-11-08 18:21:06 -0600

    Upload main files

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import React from 'react';
+import { Button, StyleSheet, Text, View, Switch, TextInput, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchValue: false,
+      newText : '',
+    }    
+  }
+
+  onChangeText = (text) => {    
+    this.setState({newText: text})    
+  }
+
+  onChange = (value) => {
+    console.warn(`El switch cambiará a: ${value}`);
+    this.setState({switchValue: value})
+  }
+
+  onPressLearnMore(){
+    Alert.alert(`${this.state.newText}`, "Es lo que escribió.") 
+  }
+
+  render() {    
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={this.onChangeText}          
+          />
+        <Switch
+          value = {this.state.switchValue}
+          onValueChange={() => this.onChange(!this.state.switchValue)}
+        />
+        <Button
+          onPress= {() => this.onPressLearnMore()}
+          title ="Learn More"
+          color="#841584"
+          accessibilityLabel='Learn more about this purple button'
+          />
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +56,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    margin: 15,
+    height: 35,
+    padding: 10,
+    borderColor: '#000',
+    borderWidth: 1
+  }
 });
